8d0e77072e26422c5eee6760decc720946e7dbdd
Hafiz <hafizalimi77@gmail.com>
2022-10-26 12:31:24 +0100

    added images

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -3,6 +3,8 @@ import { ProjectCard } from "./ProjectCard";
  
 import proj1 from "../assets/img/beachfinder.png";
 import proj2 from "../assets/img/flutter.png";
+import proj3 from "../assets/img/acebook.png";
+import proj4 from "../assets/img/makerbnb.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -21,14 +23,33 @@ export const Projects = () => {
       imgUrl: proj2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "AceBook",
+      description: "Full Stack",
+      imgUrl: proj3,
+    },
+  ];
+
+  const projects2 = [
+    {
+      title: "Makers Bnb",
+      description: "Full Stack",
+      imgUrl: proj4,
+    },
+    {
+      title: "Spotlas App",
+      description: "Mobile Development",
       imgUrl: proj2,
     },
+    {
+      title: "AceBook",
+      description: "Full Stack",
+      imgUrl: proj3,
+    },
+  ];
     // {
     //   title: "Business Startup",
     //   description: "Design & Development",
-    //   imgUrl: proj,
+    //   imgUrl: proj3,
     // },
     // {
     //   title: "Business Startup",
@@ -40,7 +61,7 @@ export const Projects = () => {
     //   description: "Design & Development",
     //   imgUrl: projImg3,
     // },
-  ];
+  
 
   return (
     <section className="project" id="project">
@@ -51,7 +72,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -79,7 +100,19 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
